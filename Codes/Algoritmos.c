ff9267c5ac8c3f2cc7d717e13a2b08c43a99aa62
ArthurDeBellis <arthur.bellis@ufv.br>
2019-06-16 15:57:00 -0300

    Códigos de ordenacao devidamente testados

---
+++ Codes/Algoritmos.c
@@ -0,0 +1,84 @@
+#include "Algoritmos.h"
+
+//Algoritmos de ordenacao
+
+void bubble_sort (int *vetor, int n) {
+    int k, j, aux;
+    for (k = n - 1; k > 0; k--) {
+        for (j = 0; j < k; j++) {
+            if (vetor[j] > vetor[j + 1]) {
+                aux = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j+1] = aux;
+            }
+        }
+    }
+}
+
+void radix_sort(int *vetor, int n) {
+	int i; 
+    int exp = 1; 
+    int m = 0; 
+    int vetAux[n]; 
+    int temp[n];
+	for(i = 0; i < n; i++) {
+		if(vetor[i] > m) {
+			m = vetor[i];
+		}
+	}
+	while((m/exp) > 0) {
+		for (i = 0; i < n; i++) {
+			vetAux[i] = 0;
+		}
+		for(i = 0; i < n; i++) {
+			vetAux[(vetor[i] / exp) % 10]++;
+		}
+		for(i = 1; i < n; i++) {
+			vetAux[i] += vetAux[i-1];
+		}
+		for(i = (n - 1); i >= 0; i--) {
+			temp[--vetAux[(vetor[i] / exp) % 10]] = vetor[i];
+		}
+		for(i = 0; i < n; i++) {
+			vetor[i] = temp[i];
+		}
+		exp *= 10;
+	}
+}
+
+void quick_sort(int *vetor, int left, int right){
+    //Entre com vetor, 0, TAM-1
+    int i, j, x, y;     
+    i = left;
+    j = right;
+    x = vetor[(left + right) / 2];
+    while(i <= j) {
+        while(vetor[i] < x && i < right) {
+            i++;
+        }
+        while(vetor[j] > x && j > left) {
+            j--;
+        }
+        if(i <= j) {
+            y = vetor[i];
+            vetor[i] = vetor[j];
+            vetor[j] = y;
+            i++;
+            j--;
+        }
+    }
+    if(j > left) {
+        quick_sort(vetor, left, j);
+    }
+    if(i < right) {
+        quick_sort(vetor, i, right);
+    }
+}
+
+//Algoritmo escolhido pelo grupo
+
+/*TODO: Escolher um algoritmo*/
+
+//Algoritmo escolhido pelo grupo modificado
+
+/*TODO: Alterar o algoritmo*/
---
+++ Codes/Algoritmos.h
@@ -0,0 +1,22 @@
+#ifndef ALGORITMOS_H
+#define ALGORITMOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 10
+
+//Algoritmos de ordenacao
+
+void bubble_sort(int *vetor, int n);
+void radix_sort(int *vetor, int n);
+void quick_sort(int *vetor, int left, int right);
+
+//Algoritmo escolhido pelo grupo
+
+/*TODO: Prototipo do algoritmo*/
+
+//Algoritmo escolhido pelo grupo modificado
+
+/*TODO: Prototipo do algoritmo alterado*/
+
+#endif
